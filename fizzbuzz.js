3e54397a871fb5697ed7a042d7e9a5aeb62b7716
Korman Carter <kormancarter@gmail.com>
2025-09-17 09:56:00 -0600

    Add files via upload

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,19 +1,21 @@
+function runFizzBuzz(limit) {
+      const results = [];
+      for (let num = 1; num <= limit; num++) {
+        if (num % 3 === 0 && num % 5 === 0) {
+          results.push("FizzBuzz");
+        } else if (num % 3 === 0) {
+          results.push("Fizz");
+        } else if (num % 5 === 0) {
+          results.push("Buzz");
+        } else {
+          results.push(num);
+        }
+      }
+      return results;
+    }
 
-function checknumber(limit) {
-    
-
-    for (let num = 1; num <= limit; num++){
-if (num % 3 == 0 && num % 5 == 0 && num <= 100){
-    console.log("FizzBuzz")
-}
-else if (num % 3 == 0 && num <= 100)
-    console.log("Fizz")
-else if ( num % 5 == 0 && num <= 100){
-    console.log("Buzz")
-}
-else{
-    console.log(num)
-}
-}
-}
-checknumber(20)
+    function showFizzBuzz() {
+      let amount = parseInt(document.getElementById("fizzbuzz-input").value);
+      document.getElementById("fizzbuzz-output").innerHTML =
+        runFizzBuzz(amount).join("<br>");
+    }
